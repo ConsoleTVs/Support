1e417abf24dab6b0eafefda2c85ba3f0a494c22f
Erik Campobadal <ConsoleTVs@gmail.com>
2017-09-09 21:30:18 +0200

    Update StringFunctions.php

--- Traits/StringFunctions.php
+++ Traits/StringFunctions.php
@@ -50,7 +50,7 @@ trait StringFUnctions
      *
      * @return string
      */
-    public function randomString(int $length = 10, string $letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ') : string
+    public static function randomString(int $length = 10, string $letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ') : string
     {
         return substr(str_shuffle(str_repeat($x = $letters, ceil($length / strlen($x)))), 1, $length);
     }
